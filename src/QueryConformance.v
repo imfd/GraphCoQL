20e06be31a25fa0cf438d45411d77400e0e66a4b
Tomas <tdiaz@imfd.cl>
2018-10-23 10:12:38 -0300

    Refactor change argumentsConform to return bool instead

--- src/QueryConformance.v
+++ src/QueryConformance.v
@@ -3,6 +3,8 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
+Require Import List.
+
 Require Import Schema.
 Require Import SchemaAux.
 Require Import Query.
@@ -16,29 +18,33 @@ Section QueryConformance.
   Implicit Type schema : @wfSchema Name Vals.
 
 
-  Definition argumentsConform schema (tname fname : Name) (α : {ffun Name -> Vals}) : Prop :=
-    forall arg value ty,
-      α arg = value ->
-      lookupArgument schema tname fname arg = Some (FieldArgument arg ty) ->
-      (hasType schema) ty value.
+  Definition argumentConforms schema (α : {ffun Name -> Vals}) (arg : FieldArgumentDefinition) : bool :=
+    match arg with
+    | (FieldArgument argname ty) => (hasType schema) ty (α argname)
+    end.
+  
 
+  Definition argumentsConform schema (α : {ffun Name -> Vals}) (args : list FieldArgumentDefinition) : bool :=
+    forallb (argumentConforms schema α) args.
+     
+    
   Inductive SelectionConforms schema : @Query Name Name Name Vals -> Name -> Prop :=
   | FieldConforms : forall tname fname α args ty,
       lookupField schema tname fname = Some (Field fname args ty) ->
-      argumentsConform schema tname fname α ->
+      argumentsConform schema α args ->
       SelectionConforms schema (SingleField fname α) tname
   | LabeledFieldConforms : forall tname fname α args ty label,
       lookupField schema tname fname = Some (Field fname args ty) ->
-      argumentsConform schema tname fname α ->
+      argumentsConform schema α args ->
       SelectionConforms schema (LabeledField label fname α) tname
   | NestedFieldConforms : forall tname fname α ϕ args ty,
       lookupField schema tname fname = Some (Field fname args ty) ->
-      argumentsConform schema tname fname α ->
+      argumentsConform schema α args ->
       SelectionConforms schema ϕ ty ->
       SelectionConforms schema (NestedField fname α ϕ) tname
   | NestedLabeledFieldConforms : forall tname fname α ϕ args ty label,
       lookupField schema tname fname = Some (Field fname args ty) ->
-      argumentsConform schema tname fname α ->
+      argumentsConform schema α args ->
       SelectionConforms schema ϕ ty ->
       SelectionConforms schema (NestedLabeledField label fname α ϕ) tname
   | InlineFragmentConforms : forall ty ty' ϕ,
@@ -52,12 +58,10 @@ Section QueryConformance.
       SelectionConforms schema (SelectionSet ϕ ϕ') ty 
   .
 
-  Definition QueryConforms schema (q : @Query Name Name Name Vals) : Prop :=
-    SelectionConforms schema q (root schema).
 
   Record wfQuery schema := WFQuery {
                               query : @Query Name Name Name Vals;
-                              _ : QueryConforms schema query
+                              queryConforms : SelectionConforms schema query (root schema)
                             }.
 
 
