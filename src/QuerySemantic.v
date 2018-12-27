173395bbf55e3cfb808035003f5c639bb46d0d98
Tomas <tdiaz@imfd.cl>
2018-12-27 11:21:42 -0300

    Add some obligation proofs for collect but missing beta index
    This case is a bit weirder... The thing is that:
    1. Indexed map had to be redefined to include the information that
        In r rs.. BUT
    2. this is not enough, because the actual collect call is made on the
        list INSIDE this r... and that information is not available (as it is
        right now)

--- src/QuerySemantic.v
+++ src/QuerySemantic.v
@@ -5,6 +5,7 @@ Unset Printing Implicit Defensive.
 Set Asymmetric Patterns.
 
 From extructures Require Import ord fmap.
+From Equations Require Import Equations.
 
 
 Require Import SchemaWellFormedness.
@@ -19,16 +20,13 @@ Require Import Query.
 Require Import Graph.
 
 
-Require Import CpdtTactics.
+Require Import Ssromega.
 
-Require Import Program.
-
-Require Recdef.
+Require Import List.
 
 Section QuerySemantic.
 
   Variables N Name Vals : ordType.
-  
   
   Notation "'ϵ'" := Empty.
   Notation "l <- 'null'" := (Null l) (at level 50).
@@ -41,12 +39,15 @@ Section QuerySemantic.
   
   Section Map.
     
-    Variables (T1 T2 : Type) (x2 : T2) (f : nat -> T1 -> T2) (s : seq T1).
-    Definition indexed_map s :=
-      let fix imap (f : nat -> T1 -> T2) (s : seq T1) i {struct s} : seq T2 :=
-          if s is hd :: tl then (f i hd) :: imap f tl (i + 1) else [::]
-      in
-      imap f s 0.
+    Variables (T1 T2 : Type) (x2 : T2) (f : nat -> T1 -> T2) (s : seq.seq T1).
+    
+    Equations indexed_map_In s (f : forall (i : nat) (x : T1), In x s -> T2) (index : nat) : seq.seq T2 :=
+      indexed_map_In [::] _ _ := [::];
+      indexed_map_In (cons hd tl) f i := cons (f i hd _) (indexed_map_In (fun i x H => f i x _) i.+1).
+
+                                
+    Equations indexed_map (s : list T1) (f : forall (i : nat) (x : T1), In x s -> T2)  : list T2 :=
+      indexed_map s f := indexed_map_In f 0.
 
   End Map.
   
@@ -57,12 +58,12 @@ Section QuerySemantic.
      response object.
 
    **)
-  Fixpoint indexed_β_filter (responses : seq (@ResponseObject Name Vals)) (filter :  @ResponseObject Name Vals) (index : nat) : seq ResponseObject :=
-    let indexed_β result filter index : seq ResponseObject :=
+  Fixpoint indexed_β_filter (responses : seq.seq (@ResponseObject Name Vals)) (filter :  @ResponseObject Name Vals) (index : nat) : seq.seq ResponseObject :=
+    let indexed_β result filter index : seq.seq ResponseObject :=
         match result, filter with
         | NestedListResult l rs, NestedListResult l' rs' =>
           if l == l' then
-            let: Results σ := nth (Results [::]) rs index in
+            let: Results σ := seq.nth (Results [::]) rs index in
             σ
           else
             [::]
@@ -80,8 +81,8 @@ Section QuerySemantic.
      
      Extracts nested results from results matching the filter response object.
    **)
-  Fixpoint β_filter (filter : @ResponseObject Name Vals) (responses : seq (@ResponseObject Name Vals)) : seq ResponseObject :=
-    let  β filter result : seq ResponseObject :=
+  Fixpoint β_filter (filter : @ResponseObject Name Vals) (responses : seq.seq (@ResponseObject Name Vals)) : seq.seq ResponseObject :=
+    let  β filter result : seq.seq ResponseObject :=
          match result, filter with
          | NestedResult l (Results χ), NestedResult l' _ => if l == l' then χ  else [::]
          | _, _ => [::]
@@ -89,7 +90,7 @@ Section QuerySemantic.
     in
     flatten [seq (β filter r) | r <- responses].
   
-  Fixpoint γ_filter (flt : @ResponseObject Name Vals) (responses : seq (@ResponseObject Name Vals)) : seq (@ResponseObject Name Vals) :=
+  Fixpoint γ_filter (flt : @ResponseObject Name Vals) (responses : seq.seq (@ResponseObject Name Vals)) : seq.seq (@ResponseObject Name Vals) :=
     let γ flt result : bool :=
         match result, flt with
         | SingleResult l v, SingleResult l' v' => (l == l') && (v == v') 
@@ -100,46 +101,89 @@ Section QuerySemantic.
         end
     in
     filter (fun r => ~~(γ flt r)) responses.
+
+  Lemma γ_nil_nil r : γ_filter r [::] = [::].
+  Proof. by case: r. Qed.
+
+
+  Lemma γ_reduce_one lst r x :
+    γ_filter r (x :: lst) = x :: (γ_filter r lst) \/
+    γ_filter r (x :: lst) = γ_filter r lst.
+  Proof.
+  Admitted.
+
   
+  Lemma γ_responses_size_reduced (lst : seq.seq ResponseObject) (r : ResponseObject) :
+    responses_size (γ_filter r lst) <= responses_size lst.
+  Proof.
+    elim: lst => [| x l' IH].
+      by case: r.
+      move: (γ_reduce_one l' r x) => [-> | ->].
+        by rewrite /responses_size /= -/(responses_size _) -/(responses_size _); ssromega.
+        by rewrite {2}/responses_size /= -/(responses_size _); ssromega.
+  Qed.
 
-  Program Fixpoint collect (responses : seq ResponseObject) {measure (responses_size responses)} : seq ResponseObject :=
-    match responses with
-    | [::] => [::]
-    | hd :: tl =>
-      match hd with
-      | NestedResult l (Results σ) => (NestedResult l (Results (collect (σ ++ (β_filter hd tl)))))
-                                       :: (collect (γ_filter hd tl))
-      | NestedListResult l rs =>
-        (NestedListResult l
-           (indexed_map
-              (fun i r =>
-                 let: Results r := r in
-                 Results (collect (r ++ (indexed_β_filter tl hd i))))
-              rs))
-          :: (collect (γ_filter hd tl))
-      | _  => hd :: (collect (γ_filter hd tl))
-      end
-    end.     
-  Admit Obligations.
+  Lemma β_responses_size_reduced (lst : seq.seq ResponseObject) (r : ResponseObject) :
+    responses_size (β_filter r lst) <= responses_size lst.
+  Proof.
+    elim: lst => [| x l' IH].
+      by case: r.
+  Admitted.
+      
+  Lemma responses_size_app (l1 l2 : seq.seq (@ResponseObject Name Vals)) : responses_size (l1 ++ l2) = responses_size l1 + responses_size l2.
+  Proof.
+    elim: l1 => [//| n l' IH].
+      by rewrite {1}/responses_size map_cat sumn_cat.
+  Qed.
 
-  Lemma collect_eq : forall r, collect r =
-                          match r with
-                          | SingleResponse _ => r
-                          | MultipleResponses hd tl =>
-                            match hd with
-                            | NestedResult l σ => MultipleResponses
-                                                   (NestedResult l (collect (app_responses σ (β_filter hd tl))))
-                                                   (collect (γ_filter hd tl))
-                            | NestedListResult l rs => MultipleResponses
-                                                        (NestedListResult l
-                                                                          (indexed_map
-                                                                             (fun i r => collect (app_responses r (indexed_β_filter tl hd i)))
-                                                                             rs))
-                                                        (collect (γ_filter hd tl))
-                            | _ => MultipleResponses hd (collect (γ_filter hd tl))
-                            end
-                          end.
-    Proof. Admitted.
+  Lemma responses_size_cons (r : @ResponseObject Name Vals) lst : responses_size (r :: lst) = response_size r + responses_size lst.
+  Proof.
+      by case: r.
+  Qed.
+  
+  Hint Resolve γ_responses_size_reduced.
+
+  Equations collect (responses : seq.seq (@ResponseObject Name Vals)) : seq.seq (@ResponseObject Name Vals) := 
+    collect responses by rec (responses_size responses) lt :=
+      collect [::] := [::] ;
+      collect (cons hd tl) <= hd => {
+                       | NestedResult l (Results σ) => (NestedResult l (Results (collect (σ ++ (β_filter hd tl)))))
+                                       :: (collect (γ_filter hd tl)) ;
+                       | NestedListResult l rs =>
+                         (NestedListResult l
+                                           (indexed_map
+                                              (fun i r (H : In r rs) =>
+                                                 let: Results r' := r in
+                                                 Results (collect (r' ++ (indexed_β_filter tl hd i))))))
+                           :: (collect (γ_filter hd tl)) ;
+                       | _  => hd :: (collect (γ_filter hd tl))
+                     }.
+  Next Obligation.
+      by rewrite {2}/responses_size /= {1}/response_size -/(responses_size _); move: tl collect (γ_responses_size_reduced tl hd) => tl collect H; ssromega.
+  Qed.
+  Next Obligation.
+      by rewrite {2}/responses_size /= {1}/response_size -/(responses_size _); move: tl collect (γ_responses_size_reduced tl hd) => tl collect H; ssromega.
+  Qed.
+  Next Obligation.
+      by rewrite {2}/responses_size /= {1}/response_size -/(responses_size _); move: tl collect (γ_responses_size_reduced tl hd) => tl collect H; ssromega.
+  Qed.
+  Next Obligation.
+     by rewrite {2}/responses_size /= {1}/response_size -/(responses_size _); move: tl collect (γ_responses_size_reduced tl hd) => tl collect H; ssromega.
+  Qed.
+  Next Obligation.
+    by rewrite responses_size_app responses_size_cons {1}/responses_size /response_size; move: tl collect (β_responses_size_reduced tl hd) => tl collect H; ssromega.
+  Qed.
+  Next Obligation.
+      by rewrite {2}/responses_size /= {1}/response_size -/(responses_size _); move: tl collect (γ_responses_size_reduced tl hd) => tl collect H; ssromega.
+  Qed.
+  Next Obligation.
+    rewrite responses_size_cons responses_size_app. {1}/responses_size /response_size.
+  Admitted.
+  Next Obligation.
+      by rewrite {2}/responses_size /= {1}/response_size -/(responses_size _); move: tl collect (γ_responses_size_reduced tl hd) => tl collect H; ssromega.
+  Qed.
+
+ 
     
     Implicit Type schema : @wfSchema Name Vals.
     Implicit Type graph : @graphQLGraph N Name Vals.
@@ -156,7 +200,7 @@ Section QuerySemantic.
             let res := eval_query schema graph u hd in
             match res with
             | inl r => r :: (loop tl)
-            | inr (Results r) => r ++ (loop tl)
+            | inr (Results r) => r ++ (loop tl)    (* I'm "lifting" the "on T { }" results to be at the same level as the others *)
             end
           end
       in
