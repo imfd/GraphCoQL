0925a556dcf3f64d2d963783c8d4c546ce203170
Tomas <tdiaz@imfd.cl>
2019-01-08 12:20:59 -0300

    Adjust normal forms, prove equiv and redefine non redundant with Equations

--- src/NRGTNF.v
+++ src/NRGTNF.v
@@ -15,7 +15,7 @@ Require Import SchemaWellFormedness.
 Require Import Schema.
 Require Import SchemaAux.
 
-Require Import Program.
+Require Import CpdtTactics.
 
 Section NRGTNF.
 
@@ -25,19 +25,24 @@ Section NRGTNF.
   Implicit Type query : @Query Name Vals.
 
     
-  
+
   Definition is_field_query query : bool :=
     if query is InlineFragment _ _ then false else true.
 
   Definition is_inline_fragment query : bool :=
     if query is InlineFragment _ _ then true else false.
-  
+
   
   Inductive GroundTypedNormalForm : QuerySet -> Prop :=
-  | GT_MultipleSelection : forall queries,
-      (all is_field_query queries) \/ (all is_inline_fragment queries) ->
-      Forall qGroundTypedNormalForm queries ->
-      GroundTypedNormalForm (SelectionSet queries)
+  | GT_SingleQuery : forall query,
+      qGroundTypedNormalForm query ->
+      GroundTypedNormalForm (SingleQuery query)
+                            
+  | GT_MultipleSelection : forall (q : Query) (q' : QuerySet),
+      ((all is_field_query (SelectionSet q q')) || (all is_inline_fragment (SelectionSet q q'))) ->
+      qGroundTypedNormalForm q ->
+      GroundTypedNormalForm q' ->
+      GroundTypedNormalForm (SelectionSet q q')
                             
   with qGroundTypedNormalForm : Query -> Prop :=
        | GT_Field : forall f args,
@@ -54,32 +59,55 @@ Section NRGTNF.
            GroundTypedNormalForm ϕ ->
            qGroundTypedNormalForm (NestedLabeledField label f args ϕ)
 
-       | GT_InlineFragment : forall t ϕ,
+       | GT_InlineFragment : forall t (ϕ : QuerySet),
            all is_field_query ϕ ->      (* repeated in next check? *)
-           GroundTypedNormalForm (SelectionSet ϕ) ->
-           qGroundTypedNormalForm (InlineFragment t (SelectionSet ϕ)).
+           GroundTypedNormalForm ϕ  ->
+           qGroundTypedNormalForm (InlineFragment t ϕ).
   
 
-  Fixpoint is_ground_typed_normal_form query_set : bool :=
-    let: SelectionSet queries := query_set in
-    ((all is_field_query queries) || (all is_inline_fragment queries))
-      &&
-    (all is_query_in_normal_form queries)
       
-  with is_query_in_normal_form query : bool :=
-          match query with
-          | NestedField _ _ ϕ => is_ground_typed_normal_form ϕ
-          | NestedLabeledField _ _ _ ϕ => is_ground_typed_normal_form ϕ
-          | InlineFragment _ (SelectionSet ϕ) => (all is_field_query ϕ) && (all is_query_in_normal_form ϕ)
-          | _ => true
-          end.
+   
+  Fixpoint is_ground_typed_normal_form (query_set : QuerySet) : bool :=
+    match query_set with
+    | SingleQuery q => is_query_in_normal_form q
+    | SelectionSet q q' => ((all is_field_query query_set) || (all is_inline_fragment query_set))
+                            &&
+                            is_query_in_normal_form q
+                            &&
+                            is_ground_typed_normal_form q'
+      
+    end
+  with
+  is_query_in_normal_form (query : Query) : bool :=
+    match query with
+    | NestedField _ _ ϕ => is_ground_typed_normal_form ϕ
+    | NestedLabeledField _ _ _ ϕ => is_ground_typed_normal_form ϕ
+    | InlineFragment _ ϕ => (all is_field_query ϕ) && is_ground_typed_normal_form ϕ
+    | _ => true
+    end.
 
 
-
-  
-
-
- 
+  Lemma normal_formP query_set : reflect (GroundTypedNormalForm query_set) (is_ground_typed_normal_form query_set).
+  Proof.
+    apply: (iffP idP).
+    + elim query_set using QuerySet_ind with
+          (P0 := fun q => (is_query_in_normal_form q) -> qGroundTypedNormalForm q); do ?[by intros; constructor].
+      - by move=> q IH /= H; constructor; apply: IH.
+      - move=> q IH q' IHq /=.
+        by move/andP=> [/andP [Hor Hq] Hq']; constructor; [| apply: IH | apply: IHq].
+      - by move=> n args ϕ IH /= H; constructor; apply: IH.
+      - by move=> l n args ϕ IH /= H; constructor; apply: IH.
+      - by move=> t ϕ IH /= /andP [Hall H]; constructor => //; apply: IH.
+    + elim query_set using QuerySet_ind with
+        (P0 := fun q => qGroundTypedNormalForm q -> (is_query_in_normal_form q)); do ?[by intros].
+      - by move=> q IH /= H; apply: IH; inversion H. 
+      - move=> q IH q' IHq H /=; inversion H.
+        simpl in H2; apply IH in H3; apply IHq in H4.
+        by rewrite andb_true_intro //; rewrite andb_true_intro //.
+      - by move=> n args ϕ IH H /=; inversion H; apply: IH.
+      - by move=> l n args ϕ IH H /=; inversion H; apply: IH.
+      - by move=> t ϕ IH H /=; inversion H; rewrite andb_true_intro //; split; [| apply IH].
+  Qed.
 
   
 
@@ -92,19 +120,29 @@ Section NRGTNF.
                        no_repeated_query tl
      end.
 
-      
-  Fixpoint is_non_redundant selection : bool :=
-    let: SelectionSet queries := selection in
-    (all is_query_non_redundant queries) && (no_repeated_query queries)
-                                         
-  with is_query_non_redundant query : bool :=
-         match query with
-         | NestedField _ _ ϕ => is_non_redundant ϕ
-         | NestedLabeledField _ _ _ ϕ => is_non_redundant ϕ
-         | InlineFragment _ ϕ => is_non_redundant ϕ
-         | _ => true
-         end.
-  
+
+  Equations is_non_redundant (queries : @QuerySet Name Vals) : bool :=
+    {
+      is_non_redundant (SingleQuery q) := is_query_non_redundant q;
+      is_non_redundant (SelectionSet q q') := is_query_non_redundant q && is_non_redundant q'
+    }
+  where
+  is_query_non_redundant (query : @Query Name Vals) : bool :=
+    {
+      is_query_non_redundant (NestedField _ _ ϕ) := is_non_redundant ϕ;
+      is_query_non_redundant (NestedLabeledField _ _ _ ϕ) := is_non_redundant ϕ;
+      is_query_non_redundant (InlineFragment _ ϕ) := is_non_redundant ϕ;
+      is_query_non_redundant _ := true
+
+    }.
+  Next Obligation.
+    elim queries using QuerySet_ind with (P0 := fun q => P0 q (is_query_non_redundant q)); do ?[by intros].
+    by move=> q IH; rewrite is_non_redundant_equation_1; apply: f.
+    by move=> q IH q' IHq; rewrite is_non_redundant_equation_2; apply: f0.
+    by move=> n args ϕ IH; rewrite is_non_redundant_helper_1_equation_3; apply: f3.
+    by move=> l n args ϕ IH; rewrite is_non_redundant_helper_1_equation_4; apply: f4.
+    by move=> t ϕ IH; rewrite is_non_redundant_helper_1_equation_5; apply: f5.
+  Defined.
 
 
     Structure normalizedSelection := NormalizedSelection {
