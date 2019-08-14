06870c1d2904aa1ab476bef0eff1731300180285
Tomas <tdiaz@imfd.cl>
2019-08-14 12:50:08 -0400

    [feat] Add lemmas about filter and grounding

--- src/theory/NRGTNFLemmas.v
+++ src/theory/NRGTNFLemmas.v
@@ -88,10 +88,91 @@ Section Theory.
         * have : forall (q : @Query Vals), q.(is_inline_fragment) -> q.(is_field) = false by case.
             by move/(_ q Htype) ->; apply_andP; [apply : (IHq ty) | apply: (IHinls ty)].
     Qed.
+
+
+    (**
+       Elimination lemma for [are_grounded_fields].
+     *)
+    Lemma are_grounded_fields_E qs :
+      are_grounded_fields s qs = all (fun q => q.(is_field)) qs && all (is_grounded s) qs.
+    Proof.
+      elim: qs => //= q qs ->.
+        by rewrite andbACA -[RHS]andbA.
+    Qed.
+
+    (**
+       This lemma states that if queries [are_grounded_fields] then they 
+       [are_grounded].
+     *)
+    Lemma are_grounded_fields_grounded φ :
+      are_grounded_fields s φ -> are_grounded s φ.
+    Proof.
+        by elim: φ => //= q φ IH /and3P [-> Hg Hgs]; apply_andP.
+    Qed.
+
+
+    Section Filter.
+
+      Transparent is_field.
+
+      (**
+         This lemma states that filtering according to a particular
+         response name preserves [are_grounded_fields].
+       *)
+      Lemma filter_preserves_field_grounding rname φ :
+        are_grounded_fields s φ ->
+        are_grounded_fields s (filter_queries_with_label rname φ).
+      Proof.
+        funelim (filter_queries_with_label rname φ) => //= /andP [Hg Hgs]; do ? apply_andP; by apply: H.
+      Qed.
+
+      (**
+         This lemma states that filtering according to a particular
+         response name preserves [are_grounded_inlines].
+       *)
+      Lemma filter_preserves_inline_grounding rname φ :
+        are_grounded_inlines s φ ->
+        are_grounded_inlines s (filter_queries_with_label rname φ).
+      Proof.
+        funelim (filter_queries_with_label rname φ) => //=.
+        simp is_grounded => /and3P [_ /andP [Hobj Hsg] Hgs].
+        apply_and3P; last by apply: H0.
+          by apply_andP; apply: filter_preserves_field_grounding.
+      Qed.
+
+      (**
+         This lemma states that filtering according to a particular
+         response name preserves [are_grounded].
+       *)
+      Lemma filter_preserves_grounding rname φ :
+        are_grounded s φ ->
+        are_grounded s (filter_queries_with_label rname φ).
+      Proof.
+        funelim (filter_queries_with_label rname φ) => //=; simp is_grounded => /=.
+        - move=> /andP [/andP [Hobj Hg] Hgs]; apply_andP.
+          * by apply_andP; apply: filter_preserves_field_grounding.
+          * by apply: filter_preserves_inline_grounding.
+
+            all: do ? case/andP.
+            all: do [intros].
+            all: do ? apply_andP.
+            all: do ? apply: H.      
+            all: do ? [by apply: filter_preserves_field_grounding].
+            all: do ? [by apply: are_grounded_fields_grounded].
+      Qed.
+      
+    End Filter.
     
   End Ground.
 
   Section NonRedundant.
+
+
+    Section Filter.
+
+      
+      
+    End Filter.
 
   End NonRedundant.
 
@@ -105,11 +186,7 @@ End Theory.
 (* Unused lemmas *)
 
 
- (* Lemma are_grounded_fields_E qs : are_grounded_fields s qs = all (fun q => q.(is_field)) qs && all (is_grounded s) qs. *)
-    (* Proof. *)
-    (*   elim: qs => //= q qs ->. *)
-    (*     by rewrite andbACA -[RHS]andbA. *)
-    (* Qed. *)
+ (* 
     
     (* Lemma are_grounded_inlines_E qs : are_grounded_inlines s qs = all (fun q => q.(is_inline_fragment)) qs && all (is_grounded s) qs. *)
     (* Proof. *)
@@ -139,4 +216,6 @@ End Theory.
     (*               all (fun q => q.(is_field)) qs)) => //. *)
     (*   - by intros => /=; case/and3P: H2 => *; apply_andP; apply: H0. *)
     (*   - by intros; rewrite H1 in Heq. *)
-    (* Qed. *)
+    (* Qed. 
+*)
+*)
