8a1e1244c10d5cedfff1c15ad6cf3f70b0625c2d
Tomas <tdiaz@imfd.cl>
2019-10-18 16:50:59 -0300

    doc: Pass on QueryConformanceLemmas

--- src/theory/QueryConformanceLemmas.v
+++ src/theory/QueryConformanceLemmas.v
@@ -26,6 +26,23 @@ Require Import Ssromega.
 
 (* end hide *)
 
+(**
+   #<div class="jumbotron">
+      <div class="container">
+        <h1 class="display-4">Query Conformance Theory</h1>
+        <p class="lead">
+         This file contains proofs about the conformance of queries. 
+        </p>         
+        <p>
+        For the  moment we only include proofs about fragment spreadibility, based on 
+        sections from the spec. The definitions of semantics, normal form, etc. are 
+        done, both in the spec and here, under the implicit assumption that queries are 
+        valid.
+        </p>
+  </div>
+</div>#
+ *)
+
 
 Section Theory.
   Transparent qresponse_name.
@@ -34,15 +51,21 @@ Section Theory.
 
 
   Variables (Scalar : eqType).
-  
+
+  (** *** Fragment spreadibility *)
+  (** ---- *)
   Section FragmentSpread.
 
     Variable (s : wfGraphQLSchema).
 
     
+    (** **** Object spreads in object scope (cf. #<a href='https://graphql.github.io/graphql-spec/June2018/##sec-Object-Spreads-In-Object-Scope'><span>&#167;</span>5.5.2.3.1</a>#)
+        ----
+     *)
+
     (**
-       
-       https://graphql.github.io/graphql-spec/June2018/#sec-Object-Spreads-In-Object-Scope
+       This lemma states that object types can spread in object scope
+       iff they are the same type.
      *)
     Lemma object_spreads_in_object_scope type_in_scope type_condition :
       is_object_type s type_in_scope ->
@@ -57,8 +80,13 @@ Section Theory.
     Qed.
 
 
+    (** **** Abstract spreads in object scope (cf. #<a href='https://graphql.github.io/graphql-spec/June2018/##sec-Abstract-Spreads-in-Object-Scope'><span>&#167;</span>5.5.2.3.2</a>#)
+       ----
+     *)
+
     (**
-       https://graphql.github.io/graphql-spec/June2018/#sec-Abstract-Spreads-in-Object-Scope
+       This lemma states that interface types spread in object scope iff
+       the object implements the interface.
      *)
     Lemma interface_spreads_in_object_scope type_in_scope type_condition :
       is_object_type s type_in_scope ->
@@ -72,8 +100,10 @@ Section Theory.
         by rewrite mem_seqI1 seqI1_Nnil.
     Qed.
 
+    (** ---- *)
     (**
-       https://graphql.github.io/graphql-spec/June2018/#sec-Abstract-Spreads-in-Object-Scope
+       This lemma states that union types spread in object scope iff
+       the object belongs to the union.
      *)
     Lemma union_spreads_in_object_scope type_in_scope type_condition :
       is_object_type s type_in_scope ->
@@ -87,8 +117,13 @@ Section Theory.
         by rewrite mem_seqI1 seqI1_Nnil.
     Qed.
  
+    (** **** Object spreads in abstract scope (cf. #<a href='https://graphql.github.io/graphql-spec/June2018/##sec-Object-Spreads-In-Abstract-Scope'><span>&#167;</span>5.5.2.3.3</a>#)
+        ----
+     *)
+
     (**
-       https://graphql.github.io/graphql-spec/June2018/#sec-Object-Spreads-In-Abstract-Scope
+       This lemma states that object types spread in interface scope iff
+       they implement the interface.
      *)
     Lemma object_spreads_in_interface_scope type_in_scope type_condition :
       is_interface_type s type_in_scope ->
@@ -102,9 +137,10 @@ Section Theory.
         by rewrite mem_seq1I seq1I_Nnil.
     Qed.
 
-    
+    (** ---- *)
     (**
-       https://graphql.github.io/graphql-spec/June2018/#sec-Object-Spreads-In-Abstract-Scope
+       This lemma states that object types spread in interface scope iff
+       they belong to the union.
      *)
     Lemma object_spreads_in_union_scope type_in_scope type_condition :
       is_union_type s type_in_scope ->
@@ -125,3 +161,12 @@ Section Theory.
 
   
 End Theory.
+
+
+(** ---- *)
+(** 
+    #<div>
+        <a href='GraphCoQL.QueryConformance.html' class="btn btn-light" role='button'>Previous ← Query Conformance</a>
+        <a href='GraphCoQL.QueryNormalForm.html' class="btn btn-info" role='button'>Continue Reading → Query Normal Form</a>
+    </div>#
+ *)
