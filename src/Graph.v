78ad1bb5d53af3ed1f300ef8bfc3ee575b74f005
Tomas <tdiaz@imfd.cl>
2019-10-21 20:07:24 -0300

    doc: Rewrite equality section a little bit in Graph

--- src/Graph.v
+++ src/Graph.v
@@ -100,9 +100,8 @@ Notation "src '⟜' lab '→' tgt" := (src, lab, tgt) (at level 20) : graph_scop
 Section Equality.
 
   (** * Equality 
-     This section deals with some SSReflect bureaucratic things needed to establish 
-     that the different components have a decidable procedure to establish equality (they belong to the 
-     SSReflect type - eqType).
+     In this section we establish that the different components of a graph 
+     have a decidable procedure to establish equality (they belong to the eqType).
 
      This is basically done by establishing isomorphisms between the different structures
      to others that already have a decidable procedure.
@@ -111,19 +110,20 @@ Section Equality.
   Variable (Scalar : eqType).
   
 
-  (** Packing and unpacking of graph fields, needed for canonical instances  **)
+  (** Labels can be transformed into pairs  *)
   Definition prod_of_label (f : @label Scalar) := let: Label l a := f in (l, a).
   Definition label_of_prod (p : string * seq (string * @Value Scalar)) := let: (l, a) := p in Label l a.
 
-  (** Cancelation lemma **)
+  (** Cancelation lemma for label *)
   Lemma can_label_of_prod : cancel prod_of_label label_of_prod.
   Proof. by case. Qed.
 
+  (** Declaring labels in eqType *)
   Canonical label_eqType := EqType label (CanEqMixin can_label_of_prod).
 
 
   
-  (** Packing and unpacking of graph nodes, needed for canonical instances **)
+  (** Nodes can be transformed into pairs  *)
   Definition prod_of_node (n : @node Scalar) := let: Node t f := n in (t, f).
   Definition node_of_prod (p : string * seq (label * @Value Scalar)) :=
     let: (t, f) := p in Node t f.
@@ -132,12 +132,13 @@ Section Equality.
   Definition prod_of_nodeK : cancel prod_of_node node_of_prod.
   Proof. by case. Qed.
 
-  
+
+  (** Declaring nodes in eqType *)
   Canonical node_eqType := EqType node (CanEqMixin prod_of_nodeK).
   
  
 
-   (** Packing and unpacking for graphs, needed for canonical instances **)
+  (** Graphs can be transformed into pairs *)
   Definition prod_of_graph (g : @graphQLGraph Scalar) := let: GraphQLGraph r e := g in (r, e).
   Definition graph_of_prod (p : node * seq (node * label * node)) :=
     let: (r, e) := p in @GraphQLGraph Scalar r e.
@@ -146,7 +147,8 @@ Section Equality.
   (** Cancelation lemma for a graph **)
   Definition prod_of_graphK : cancel prod_of_graph graph_of_prod.
   Proof. by case. Qed.
-  
+
+  (** Declaring graphs in eqType *)
   Canonical graph_eqType := EqType graphQLGraph (CanEqMixin prod_of_graphK).
   
 
